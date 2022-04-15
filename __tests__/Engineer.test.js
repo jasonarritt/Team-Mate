8a5ba3354c498b8130a1635344526a94e08a38ea
Jason Arritt <jsn.arritt@gmail.com>
2022-04-15 12:54:15 -0400

    Engineer tests pass

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -9,3 +9,33 @@ test('should create a engineer object', () => {
     expect(engineer.email).toEqual(expect.stringContaining('@'));
     expect(engineer.github).toEqual(expect.anything());
 })
+
+test('should return engineer name as Josh', () => {
+    const engineer = new Engineer('Josh', 1, 'j@mail.com', 'jasonarritt');
+
+    expect(engineer.getName()).toBe('Josh');
+})
+
+test('should return engineer employeeID as 10', () => {
+    const engineer = new Engineer('Jason', 10, 'j@mail.com', 'jasonarritt');
+
+    expect(engineer.getID()).toBe(10);
+})
+
+test('should return engineer email as email@email.net', () => {
+    const engineer = new Engineer('Jason', 1, 'email@email.net', 'jasonarritt');
+
+    expect(engineer.getEmail()).toBe('email@email.net');
+})
+
+test('should return engineer github as jarritt', () => {
+    const engineer = new Engineer('Jason', 1, 'j@mail.com', 'jarritt');
+
+    expect(engineer.getGithub()).toBe('jarritt');
+})
+
+test('should return engineer role as Engineer', () => {
+    const engineer = new Engineer('Jason', 1, 'mail@mail.com', 'jasonarritt');
+
+    expect(engineer.getRole()).toBe('Engineer');
+})
