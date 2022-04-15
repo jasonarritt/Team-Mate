2c4d922288dd9f3753a821466aa40591c92f5bd4
Jason Arritt <jsn.arritt@gmail.com>
2022-04-15 12:37:29 -0400

    5 employee tests pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,5 +7,28 @@ test('should create a employee object', () => {
     expect(employee.name).toBe('Jason');
     expect(employee.employeeID).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.stringContaining('@'));
-    // expect(employee.officeNumber).toEqual(expect.any(number));
+})
+
+test('should return employee name as Josh', () => {
+    const employee = new Employee('Josh', 1, 'j@mail.com');
+
+    expect(employee.getName()).toBe('Josh');
+})
+
+test('should return employeeID as 10', () => {
+    const employee = new Employee('Jason', 10, 'j@mail.com');
+
+    expect(employee.getID()).toBe(10);
+})
+
+test('should return employee email as email@email.net', () => {
+    const employee = new Employee('Jason', 1, 'email@email.net');
+
+    expect(employee.getEmail()).toBe('email@email.net');
+})
+
+test('should return role as Employee', () => {
+    const employee = new Employee('Jason', 1, 'mail@mail.com');
+
+    expect(employee.getRole()).toBe('Employee');
 })
