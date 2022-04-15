eb41e18a22b45177d2d27f426166b00ccd5f8ac5
Jason Arritt <jsn.arritt@gmail.com>
2022-04-15 12:59:09 -0400

    Intern tests pass

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -9,3 +9,33 @@ test('should create a intern object', () => {
     expect(intern.email).toEqual(expect.stringContaining('@'));
     expect(intern.school).toEqual(expect.anything());
 })
+
+test('should return intern name as Josh', () => {
+    const intern = new Intern('Josh', 1, 'j@mail.com', 'UNC');
+
+    expect(intern.getName()).toBe('Josh');
+})
+
+test('should return intern employeeID as 10', () => {
+    const intern = new Intern('Jason', 10, 'j@mail.com', 'UNC');
+
+    expect(intern.getID()).toBe(10);
+})
+
+test('should return intern email as email@email.net', () => {
+    const intern = new Intern('Jason', 1, 'email@email.net', 'UNC');
+
+    expect(intern.getEmail()).toBe('email@email.net');
+})
+
+test('should return intern github as jarritt', () => {
+    const intern = new Intern('Jason', 1, 'j@mail.com', 'NC State');
+
+    expect(intern.getSchool()).toBe('NC State');
+})
+
+test('should return intern role as Intern', () => {
+    const intern = new Intern('Jason', 1, 'mail@mail.com', 'UNC');
+
+    expect(intern.getRole()).toBe('Intern');
+})
