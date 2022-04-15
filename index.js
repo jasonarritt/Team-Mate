fce2dbde0c1347fc6389ab99d1f0306e05218823
Jason Arritt <jsn.arritt@gmail.com>
2022-04-15 13:15:19 -0400

    Added js requirements to index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const inquirer = require('inquirer');
+const employee = require('./lib/Employee');
+const engineer = require('./lib/Engineer');
+const intern = require('./lib/Intern');
+const manager = require('./lib/Manager');
