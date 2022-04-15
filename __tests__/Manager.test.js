b3809d4465c646e8cc9f8e7b31a969d4f2e86b40
Jason Arritt <jsn.arritt@gmail.com>
2022-04-15 12:47:50 -0400

    Manager tests pass

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -10,3 +10,32 @@ test('should create a manager object', () => {
     expect(manager.officeNumber).toEqual(expect.anything());
 })
 
+test('should return manager name as Josh', () => {
+    const manager = new Manager('Josh', 1, 'j@mail.com', 'D-3256');
+
+    expect(manager.getName()).toBe('Josh');
+})
+
+test('should return manager employeeID as 10', () => {
+    const manager = new Manager('Jason', 10, 'j@mail.com', 'D-3256');
+
+    expect(manager.getID()).toBe(10);
+})
+
+test('should return manager email as email@email.net', () => {
+    const manager = new Manager('Jason', 1, 'email@email.net', 'D-3256');
+
+    expect(manager.getEmail()).toBe('email@email.net');
+})
+
+test('should return manager officeNumber as C-1325', () => {
+    const manager = new Manager('Jason', 1, 'j@mail.com', 'C-1325');
+
+    expect(manager.getOfficeNumber()).toBe('C-1325');
+})
+
+test('should return manager role as Manager', () => {
+    const manager = new Manager('Jason', 1, 'mail@mail.com', 'D-3256');
+
+    expect(manager.getRole()).toBe('Manager');
+})
