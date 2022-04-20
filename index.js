6f02ee4b9e95242ce78b4f2abca7f7a8661aa9d5
Jason Arritt <jsn.arritt@gmail.com>
2022-04-20 18:08:17 -0400

    Added switch for menu choices

--- index.js
+++ index.js
@@ -5,120 +5,12 @@ const engineer = require('./lib/Classes/Engineer');
 const intern = require('./lib/Classes/Intern');
 const manager = require('./lib/Classes/Manager');
 
-// Manager questions
-managerQuestionsArray = [
-    // Get manager's name
-    {
-        type: 'text',
-        name: 'name',
-        message: "Please enter the team manager's name:",
-        validate: nameInput => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log('Please enter the name of the team manager!');
-              return false;
-            }
-        }
-    },
-    // Get manager's employeeID
-    {
-        type: 'text',
-        name: 'employeeID',
-        message: "Please enter the team manager's employee ID:",
-        validate: employeeIDInput => {
-            if (employeeIDInput) {
-              return true;
-            } else {
-              console.log('Please enter the employee ID of the team manager!');
-              return false;
-            }
-        }
-    },
-    // Get manager's email
-    {
-        type: 'text',
-        name: 'email',
-        message: "Please enter the team manager's email:",
-        validate: emailInput => {
-            if (emailInput) {
-              return true;
-            } else {
-              console.log('Please enter the email address of the team manager!');
-              return false;
-            }
-        }
-    },
-    // Get manager's officeNumber
-    {
-        type: 'text',
-        name: 'officeNumber',
-        message: "Please enter the team manager's office number:",
-        validate: officeNumberInput => {
-            if (officeNumberInput) {
-              return true;
-            } else {
-              console.log('Please enter the office number of the team manager!');
-              return false;
-            }
-        }
-    },
-    // Check for adding another employee
-    {
-        type: 'confirm',
-        name: 'addEmployee',
-        message: "Would you like to add another employee?",
-        validate: addEmployeeIDInput => {
-            if (addEmployeeIDInput) {
-              return true;
-            } else {
-              console.log('Please enter the employee ID of the team manager!');
-              return false;
-            }
-        }
-    },
-    // Get manager's officeNumber
-    {
-        type: 'list',
-        name: 'employeeType',
-        message: "Please choose the type of employee you want to add:",
-        choices: ['Engineer', 'Intern', "I don't want to add another employee"]
-    },
-    
-];
-
-// Engineer questions
-engineerQuestionsArray = [
-    // Get engineer's name
-    {
-        type: 'text',
-        name: 'name',
-        message: "Please enter the engineer's name:"
-    },
-    // Get engineer's employeeID
-    {
-        type: 'text',
-        name: 'employeeID',
-        message: "Please enter the engineer's employee ID:"
-    },
-    // Get engineer's email
-    {
-        type: 'text',
-        name: 'email',
-        message: "Please enter the engineer's email:"
-    },
-    // Get engineer's officeNumber
-    {
-        type: 'text',
-        name: 'github',
-        message: "Please enter the engineer's GitHub username:"
-    }
-]
+const { mainMenuQuestions } = require('./lib/questions')
 
 
 
 
-class teamBuilder {
+class TeamBuilder {
     constructor() {
         this.manager = [];
         this.allEmployees = [];
@@ -127,14 +19,40 @@ class teamBuilder {
 
     async mainMenu() {
 
-        const {} = await inquirer.prompt();
+        const { menuChoice } = await inquirer.prompt(mainMenuQuestions);
+
+        switch (menuChoice) {
+            case 'Engineer':
+
+            break;
+
+            case 'Intern':
+
+                break;
+
+            case 'Manager':
+
+                break;
+
+            case "I'm finished adding team members.":
+
+                break;
+
+            default:
+                return;
+        }
 
     }
 
     init() {
-        console.log("Welcome to Team Builder");
+        console.log("Welcome to Managers Mate!\n");
         
         this.mainMenu();
     }
+
+
 }
 
+const team1 = new TeamBuilder();
+
+team1.init();
