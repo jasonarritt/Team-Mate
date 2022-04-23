0d0e4507ca27ea71470ea0545298a80c8f4b114d
Jason Arritt <jsn.arritt@gmail.com>
2022-04-23 15:43:49 -0400

    Updated HTML generation and style

--- src/generatehtml.js
+++ src/generatehtml.js
@@ -5,60 +5,76 @@ const generateHTML = (allEngineers, allInterns, allManagers) => (
     
     `
     <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Mate</title>
-</head>
-<body>
-    <h1>Team Mate</h1>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+        <title>Team Mate</title>
+    </head>
+    <body>
+        <h1 class="title is-1 has-text-link has-text-centered">Team Mate</h1>
 
-    <h3>Managers</h3>
-    <ul id='manager-list'>
-        ${allManagers.map(manager => (
+        <div id='managers' class='is-primary has-text-centered'>
+        <h3 class="title is-3 has-text-success">Managers</h3>
+        <section id='manager-list' class='container is-flex-direction-row is-justify-content-space-around'>
+            ${allManagers.map(({ teammateName, teammateID, teammateEmail, managerOfficeNumber }) => {
+                return`
+                <div class='manager card m-auto'>
+                <p>Name: ${teammateName}</p>
+                <p>ID: ${teammateID}</p>
+                <p>Email: <a href="mailto:${teammateEmail}">${teammateEmail}</a></p>
+                <p>Office Number: ${managerOfficeNumber}</p>
+                </div>
+                `
+                })
+            } 
+            
+        </section>
+        
+        </div>
+
+        <div id='engineers' class='is-primary has-text-centered'>
+        <h3 class="title is-3 has-text-warning">Engineers</h3>
+        <section id='engineer-list' class='container is-flex-direction-row is-justify-content-space-around'>
+
+        ${allEngineers.map(({ teammateName, teammateID, teammateEmail, engineerGithub }) => {
+            return`
+            <div class='engineer card m-auto'>
+            <p>Name: ${teammateName}</p>
+            <p>ID: ${teammateID}</p>
+            <p>Email: <a href="mailto:${teammateEmail}">${teammateEmail}</a></p>
+            <p>Office Number: ${engineerGithub}</p>
+            </div>
             `
-            <ul>
-            <li>Name: ${manager.teammateName}</li>
-            <li>ID: ${manager.teammateID}</li>
-            <li>Email: <a href="mailto:${manager.teammateEmail}">${manager.teammateEmail}</a></li>
-            <li>Office Number: ${manager.managerOfficeNumber}</li>
-            </ul>
+            })
+        } 
+        
+        </section>
+        </div>
+
+        <div id='interns' class='is-primary has-text-centered'>
+        <h3 class="title is-3 has-text-info">Interns</h3>
+        <section id='intern-list' class='container is-flex-direction-row is-justify-content-space-around'>
+
+        ${allInterns.map(({ teammateName, teammateID, teammateEmail, internSchool }) => {
+            return`
+            <div class='intern card m-auto'>
+            <p>Name: ${teammateName}</p>
+            <p>ID: ${teammateID}</p>
+            <p>Email: <a href="mailto:${teammateEmail}">${teammateEmail}</a></p>
+            <p>Office Number: ${internSchool}</p>
+            </div>
             `
-            ))}
-    </ul>
-    
-    <h3>Engineers</h3>
-    <ul>
-    ${allEngineers.map(engineer => (
-        `
-        <ul>
-        <li>Name: ${engineer.teammateName}</li>
-        <li>ID: ${engineer.teammateID}</li>
-        <li>Email: <a href="mailto:${engineer.teammateEmail}">${engineer.teammateEmail}</a></li>
-        <li>Github: <a href='https://github.com/${engineer.engineerGithub}' target='_blank'>${engineer.engineerGithub}</a></li>
-        </ul>
-        `
-        ))}
-    </ul>
+            })
+        } 
+        
+        </section>
+        </div>
 
-    <h3>Interns</h3>
-    <ul>
-    ${allInterns.map(intern => (
-        `
-        <ul>
-        <li>${intern.teammateName}</li>
-        <li>${intern.teammateID}</li>
-        <li>Email: <a href="mailto:${intern.teammateEmail}">${intern.teammateEmail}</a></li>
-        <li>${intern.internSchool}</li>
-        </ul>
-        `
-        ))}
-    </ul>
-
-</body>
-</html>
+    </body>
+    </html>
 
     `
 )
